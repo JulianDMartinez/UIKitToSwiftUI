05f01b1c8b029dc9e798b1e85b0f2846cdae236d
Julian Martinez <julian.martinez.s@outlook.com>
2022-05-30 16:35:32 -0400

    Initial Commit

---
+++ UIKitToSwiftUI/UIKitToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitToSwiftUIApp.swift
+//  UIKitToSwiftUI
+//
+//  Created by Julian Martinez on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct UIKitToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
